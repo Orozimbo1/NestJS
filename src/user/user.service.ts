3960007f221702393879a7812c52a87d145a8594
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2024-02-27 23:06:19 -0300

    delete e update

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-users.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UpdatePutUserDTO } from './dto/update-put-users.dto';
+import { UpdatePatchUserDTO } from './dto/update-patch-users.dto';
 
 @Injectable()
 export class UserService {
@@ -22,5 +24,43 @@ export class UserService {
         id,
       },
     });
+  }
+
+  async update(id: number, data: UpdatePutUserDTO) {
+    await this.exists(id);
+
+    return this.prisma.user.update({
+      data,
+      where: {
+        id,
+      },
+    });
+  }
+
+  async updatePartial(id: number, data: UpdatePatchUserDTO) {
+    await this.exists(id);
+
+    return this.prisma.user.update({
+      data,
+      where: {
+        id,
+      },
+    });
+  }
+
+  async delete(id: number) {
+    await this.exists(id);
+
+    return this.prisma.user.delete({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async exists(id: number) {
+    if (!(await this.show(id))) {
+      throw new NotFoundException(`O usuário ${id} não existe.`);
+    }
   }
 }
