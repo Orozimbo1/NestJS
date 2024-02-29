d4737322abb8be79aec0699a47fff9bed4e98bff
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2024-02-28 21:21:41 -0300

    Auth Controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { UserModule } from 'src/user/user.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   imports: [
     JwtModule.register({
       secret: `64byl$4\\/}#Nx2Qixd(]k~g"M%pLyggW`,
     }),
+    UserModule,
+    PrismaModule,
   ],
+  controllers: [AuthController],
 })
 export class AuthModule {}
---
+++ src/auth/dto/auth-register.dto.ts
@@ -0,0 +1,3 @@
+import { CreateUserDTO } from 'src/user/dto/create-users.dto';
+
+export class AuthRegisterDTO extends CreateUserDTO {}
