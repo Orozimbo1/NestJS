2cbcc698858962fee60dcfd8b5b8622aa148e96b
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2024-02-28 22:32:34 -0300

    Assinatura de token JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,15 +3,11 @@ import { AuthLoginDTO } from './dto/auth-login.dto';
 import { AuthRegisterDTO } from './dto/auth-register.dto';
 import { AuthForgetDTO } from './dto/auth-forget.dto';
 import { AuthResetDTO } from './dto/auth-reset.dto';
-import { UserService } from 'src/user/user.service';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(
-    private readonly userService: UserService,
-    private readonly authService: AuthService,
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
   @Post('login')
   async login(@Body() { email, password }: AuthLoginDTO) {
@@ -20,7 +16,7 @@ export class AuthController {
 
   @Post('register')
   async register(@Body() body: AuthRegisterDTO) {
-    return this.userService.create(body);
+    return this.authService.register(body);
   }
 
   @Post('forget')
